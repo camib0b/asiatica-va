deec871c7d41bfadadff3af64e304b13b6e2e528
camib0b <camilaescudero@uc.cl>
2026-01-18 19:43:29 -0300

    feat-added slower and reset speed buttons to controlbar

--- build/AVA_autogen/EWIEGA46WW/moc_MainWindow.cpp
+++ build/AVA_autogen/EWIEGA46WW/moc_MainWindow.cpp
@@ -46,6 +46,8 @@ template <> constexpr inline auto MainWindow::qt_create_metaobjectdata<qt_meta_t
         "onSeekBackwardClicked",
         "onSeekForwardClicked",
         "onFasterClicked",
+        "onSlowerClicked",
+        "onResetSpeedClicked",
         "onMuteToggled",
         "muted"
     };
@@ -63,9 +65,13 @@ template <> constexpr inline auto MainWindow::qt_create_metaobjectdata<qt_meta_t
         QtMocHelpers::SlotData<void()>(6, 2, QMC::AccessPrivate, QMetaType::Void),
         // Slot 'onFasterClicked'
         QtMocHelpers::SlotData<void()>(7, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'onSlowerClicked'
+        QtMocHelpers::SlotData<void()>(8, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'onResetSpeedClicked'
+        QtMocHelpers::SlotData<void()>(9, 2, QMC::AccessPrivate, QMetaType::Void),
         // Slot 'onMuteToggled'
-        QtMocHelpers::SlotData<void(bool)>(8, 2, QMC::AccessPrivate, QMetaType::Void, {{
-            { QMetaType::Bool, 9 },
+        QtMocHelpers::SlotData<void(bool)>(10, 2, QMC::AccessPrivate, QMetaType::Void, {{
+            { QMetaType::Bool, 11 },
         }}),
     };
     QtMocHelpers::UintData qt_properties {
@@ -96,7 +102,9 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 3: _t->onSeekBackwardClicked(); break;
         case 4: _t->onSeekForwardClicked(); break;
         case 5: _t->onFasterClicked(); break;
-        case 6: _t->onMuteToggled((*reinterpret_cast<std::add_pointer_t<bool>>(_a[1]))); break;
+        case 6: _t->onSlowerClicked(); break;
+        case 7: _t->onResetSpeedClicked(); break;
+        case 8: _t->onMuteToggled((*reinterpret_cast<std::add_pointer_t<bool>>(_a[1]))); break;
         default: ;
         }
     }
@@ -121,14 +129,14 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 7)
+        if (_id < 9)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 7;
+        _id -= 9;
     }
     if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 7)
+        if (_id < 9)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 7;
+        _id -= 9;
     }
     return _id;
 }
