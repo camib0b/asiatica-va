f98c301d4c2889c7a9f523ff39953b4e271dba86
camib0b <camilaescudero@uc.cl>
2026-05-25 18:06:28 -0400

    Increase Circle Entry default lead time by 3 seconds.
    Factory default lead is now 8s (was 5s); lag remains 6s.

--- AVA_V01_cpp/state/EventDefaults.cpp
+++ AVA_V01_cpp/state/EventDefaults.cpp
@@ -18,7 +18,7 @@ const QHash<QString, EventDuration>& defaultDurationTable() {
       {QStringLiteral("PC Foul"),      {5000, 2000}},
       {QStringLiteral("Card"),         {6000, 3000}},
       {QStringLiteral("Pass"),         {3000, 5000}},
-      {QStringLiteral("Circle Entry"), {5000, 6000}},
+      {QStringLiteral("Circle Entry"), {8000, 6000}},
       {QStringLiteral("16-yd"),        {3000, 10000}},
       {QStringLiteral("50-yd"),        {3000, 8000}},
       {QStringLiteral("75-yd"),        {3000, 8000}},
