befdd4db85bd9893a6bf23acb173bfae28125ec7
camib0b <camilaescudero@uc.cl>
2026-08-18 23:04:20 -0400

    Stop tracking local build and clangd cache directories.
    These paths are already covered by .gitignore; removing them from the index keeps git status clean after local builds.

--- AVA_V01_cpp/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_AVA_FILE /Users/cam/dev/asiatica-va/AVA_V01_cpp/build/AVA.app/Contents/MacOS/AVA)
-set(__QT_DEPLOY_TARGET_AVA_TYPE EXECUTABLE)
--- AVA_V01_cpp/build/CMakeFiles/AVA.dir/build/.qt/rcc/qrc_ava_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/AVA.dir/build/.qt/rcc/qrc_ava_resources.cpp.o: \
-  /Users/cam/dev/asiatica-va/AVA_V01_cpp/build/.qt/rcc/qrc_ava_resources.cpp
